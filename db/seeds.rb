bf7e34aba7be22a00feaa25d165ef4e2e1eb178f
clairedeboer <claire.deboer@gmail.com>
2021-02-15 21:02:42 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,24 @@ Question.destroy_all
 User.destroy_all
 Answer.destroy_all
 
-Question.create(question:'Is the earth flat?!', option1: 'Yes', option2: 'No', reward: 100)
-10.times do 
-Question.create(question:'Do you shop more during the daytime or the evening?', option1: 'Daytime', option2: 'Evening', reward: 200)
-end
+Question.create(creator_id: 1, question:'Is the earth flat?!', option1: 'Yes', option2: 'No', reward: 100)
+Question.create(creator_id: 1, question:'Do you shop more during the daytime or the evening?', option1: 'Daytime', option2: 'Evening', reward: 200)
+Question.create(creator_id: 1, question:'What is your favorite coding framework?', option1: 'Rails', option2: 'React', reward: 50)
+Question.create(creator_id: 1, question:'Are you planning on buying a mattress in the next six months?', option1: 'Yes', option2: 'No', reward: 25)
+Question.create(creator_id: 1, question:'What is your favorite neighborhood in Brooklyn?', option1: 'Ditmas Park', option2: 'Red Hood', reward: 45)
+Question.create(creator_id: 1, question:'What is your credit card score?', option1: 'Between 300-599', option2: 'Between 600-850', reward: 200)
+Question.create(creator_id: 1, question:'What career should I have?', option1: 'Piano Player', option2: 'Banker', reward: 100)
+Question.create(creator_id: 1, question:'Do you like my haircut?', option1: 'Absolutely', option2: 'No', reward: 200)
+Question.create(creator_id: 1, question:'Who was the best president of the United States?', option1: 'Lincoln', option2: 'JFK', reward: 75)
+Question.create(creator_id: 1, question:'What is your favorite scent?', option1: 'Rosebuds', option2: 'Sharpie', reward: 60)
 
-5.times do 
-    User.create(name: 'agent smith', age: 36, username: 'smith', password: "123")
-end
+User.create(name: 'Agent Smith', age: 36, ethnicity: "African American", gender_identity: "Male", political_party: "Libertarian", location: "Milwaukee, WI", religion: "Christian", currency: 100, username: 'smith', password: "123")
+User.create(name: 'Joann', age: 65, ethnicity: "Caucasian", gender_identity: "Female", political_party: "Democratic", location: "Brooklyn, NY", religion: "Jewish", currency: 200, username: 'joann', password: "123")
+User.create(name: 'Tony', age: 45, ethnicity: "Caucasian", gender_identity: "Male", political_party: "Republican", location: "North Caldwell, NJ", religion: "Catholic", currency: 500, username: 'tony', password: "123")
+
 questions = Question.all
 users = User.all
- answers = ['option1', 'option2']
+answers = ['option1', 'option2']
 
 30.times do 
    x = Answer.create(user_id: 1, question_id: 1, response: answers.shuffle.first )
